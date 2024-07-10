51563edc5b3c62de69581d102c5f756f4c9abce6
André Santos <andrerfcsantos@gmail.com>
2024-07-10 07:17:14 +0100

    Remove vercel analytics

--- src/services/events.js
+++ src/services/events.js
@@ -1,5 +1,9 @@
 export function sendEvent(category, action, value) {
-  if (!Array.isArray(window._paq) && window._paq.push) {
-    window._paq.push(["trackEvent", category, action, value]);
+  if (typeof window.gtag === "function") {
+    window.gtag("event", action, {
+      event_category: category,
+      event_label: `${action} - ${value}`,
+      value: value,
+    });
   }
 }
